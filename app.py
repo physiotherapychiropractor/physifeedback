860e06cf707ba842d9ea63068ebd052ff59db0c5
Hadiya Firdaus <hfirdaus@benevity.com>
2021-06-11 11:06:38 -0600

    Hook up code pen and run test classifer on live video data

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import numpy as np
+import sys
+
+from model.classifier import model, smoother
 
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
+@app.route('/demo')
+def demo():
+    return render_template('demo/demo.html')
+
+
+@app.route('/getpose', methods=['POST'])
+def getpose():
+    pose_classification = ''
+    if request.method == 'POST':
+        json = request.json
+        pose_landmarks = np.array(
+            [[lmk['x'] * json['width'], lmk['y'] * json['height'], lmk['z'] * json['width']]
+             for lmk in json['pose_landmarks']],
+            dtype=np.float32)
+        pose_classification = model(pose_landmarks)
+    # print(pose_classification, file=sys.stderr)
+    return pose_classification
 
 
 if __name__ == '__main__':
